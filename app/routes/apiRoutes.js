150b62f24d027ff8247fdabe09c00b0add683860
Nicholas N <nnlaor@gmail.com>
2017-05-01 07:41:24 -0700

    restarting

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,12 +1,24 @@
-var poke = require('../data/friends.js');
+var poke = require('../data/friends');
 
 
 module.exports = function(app){
 	app.get('/api/friends', function(req, res){
-		res.json(friends);
+		res.json(poke);
 	});
 
 	app.post('/api/friends', function(req, res){
+		var match = {
+			name: '', 
+			phoot: '', 
+			diff: 100 
+		};
+
+		var newUser = req.body;
+		var userScores = user.scores;
+		var totalDiff = 0;
+
+
+
 
 	});
 
